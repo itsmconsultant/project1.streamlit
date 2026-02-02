28c9f80deb2efc8faf5262ccb5b6c04316a519a8
itsmconsultant <sm.consultant.it@gmail.com>
2026-02-02 20:50:50 +0700

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -4,7 +4,7 @@ from sqlalchemy import text
 def show_run_procedure(conn): # Tetap terima conn jika perlu, tapi kita buat koneksi baru di sini
     st.title("⚙️ Jalankan Proses Data")
     
-    selected_date = st.date_input("Pilih Tanggal :")
+    selected_date = st.date_input("Pilih Tanggal Data yang akan di proses :")
     
     if st.button("Proses Data"):
         # Streamlit akan otomatis mencari [connections.postgresql] di Secrets
